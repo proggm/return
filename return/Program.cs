6bb36297d736ec5c1528acf3d6bb19f15e112611
viktor <moiseev0506@mail.ru>
2022-10-27 21:00:56 +0300

    version 1

---
+++ return/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @return
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кол-во патронов в магазине: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество магазинов: ");             
+            int second=int.Parse(Console.ReadLine());    
+            
+            Console.WriteLine($"Всего патронов: " + Return(first,second));
+
+            Console.ReadKey();            
+        }
+        public static int Return(int one, int two)
+        {
+            if (one >30)
+            {
+                return 0;
+            }
+            else
+            {
+                return one * two;
+            }
+        }
+
+        
+        
+        
+         
+        
+       
+    }
+}
